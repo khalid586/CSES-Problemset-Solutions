4d78263b99140da416ebbb7403baf003c35b1300
khalid <khalidcomilla58@gmail.com>
2023-04-05 09:11:01 +0600

    AdHoc problem

--- 1.Intoductory/CSES Trailing Zeros.cpp
+++ 1.Intoductory/CSES Trailing Zeros.cpp
@@ -1,49 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-typedef long long             ll;
-typedef deque<ll>             dq ;
-typedef list<ll>              li;
-typedef vector<string>        vst ;
-typedef vector<ll>            vll ;
-typedef deque<ll>::iterator   dita;
-typedef double                dd;
-typedef greater<ll>           gt;
-typedef list<ll>::iterator    liita;
-typedef vector<ll>::iterator  vecita;
-#define immortal_slave  int main()
-#define in(x)           cin>>x;
-#define khalid          std::ios::sync_with_stdio(false);cin.tie(NULL);
-#define mp              make_pair
-#define out(x)          cout<<x<<"\n";
-#define out2(x)         cout<<x;
-#define newline         cout<<"\n";
-#define pb              push_back
-#define pf              push_front
-#define pob             pop_back
-#define pof             pop_front
-#define asc_sort(v)     sort(v.begin(), v.end())
-#define dsc_sort(v)     sort(v.begin(), v.end(),gt())
-#define rep(a,b)        for(ll i = a; i < b;++i)
-#define repj(j,a,b)     for(ll j = a; j < b;++j)
-#define rep2(a,b)       for(ll i = a; i <= b; ++i)
-#define rep2j(j,a,b)    for(ll j = a; j <= b; ++j)
-#define rep3(a,b)       for(ll i = a; i >= b;--i)
-#define rep3j(j,a,b)    for(ll j = a; j >= b;--j)
-#define ss              stringstream
-#define st              string
-#define min3(a,b,c)     min( a,min( b, c))
-#define min4(a,b,c,d)   min( d,min3( a, b, c))
-#define max3(a,b,c)     max( a,max( b, c))
-#define max4(a,b,c,d)   max( d,max3(a,b,c))
-#define sum(v)          accumulate(v.begin(),v.end(),0ll)
-#define lcm(a,b)        (a*b)/__gcd(a,b)
-#define all(x)          x.begin(),x.end()
- 
 //Starting in the name of Allah(THE ALMIGHTY)
 void FastIO()
 {
-    khalid
     #ifndef ONLINE_JUDGE
         freopen("in.txt","r",stdin);
       //  freopen("out.txt","w",stdout);
@@ -51,14 +11,14 @@ void FastIO()
 }
 
 
-immortal_slave
+int main()
 {
     FastIO();
-    ll n,ans=0; in(n)
+    int n , ans = 0 ; cin >> n;
 
     while(n>=5){ ans+=(n/5); n/=5;}
     
-    out(ans)
+    cout << ans << '\n';
  
     return 0;
 }
